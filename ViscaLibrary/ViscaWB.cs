5d2de0040c713690e9a2cca79c160e60ab9de534
Maxim Batourine <m.batourine@gmail.com>
2021-03-07 10:05:57 -0500

    Added universal WB class ViscaWBMode

--- ViscaLibrary/ViscaWB.cs
+++ ViscaLibrary/ViscaWB.cs
@@ -27,6 +27,46 @@ namespace Visca
         }
     }
 
+    public class ViscaWBMode : ViscaCommand 
+    {
+        private readonly ViscaVariable _mode;
+
+        public ViscaWBMode(byte address, WB mode)
+            : base(address)
+        {
+            _mode = new ViscaVariable("WB Modea", (byte)mode.Key);
+
+            Append(new byte[]{
+                Visca.Category.Camera1,
+                Visca.Commands.WB
+            });
+            Append(_mode);
+        }
+
+        public WB Mode
+        {
+            get { return WB.GetByKey(_mode.Value); }
+            set { _mode.Value = (byte)value.Key; }
+        }
+
+        /// <summary>
+        /// Passthrough setter of mode suitable to enqueue command in one operation
+        /// </summary>
+        /// <param name="mode">WB Mode to set</param>
+        /// <returns>Same object</returns>
+        public ViscaWBMode SetMode(WB mode)
+        {
+            Mode = mode;
+            return this;
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Camera{0} WB.{1}", this.Destination, WB.GetByKey(_mode.Value).Value);
+        }
+
+    }
+
     public class ViscaWBAuto : ViscaCommand
     {
         public ViscaWBAuto(byte address)
