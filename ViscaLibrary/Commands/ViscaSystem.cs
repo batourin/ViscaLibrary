34974d2d3ca43319a9f870aa5bd83aaa3145348c
Maxim Batourine <m.batourine@gmail.com>
2021-03-30 17:38:59 -0400

    Add ViscaClear

---
+++ ViscaLibrary/Commands/ViscaSystem.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Visca
+{
+    public class ViscaClear : ViscaCommand
+    {
+        public ViscaClear()
+            : base()
+        {
+            Append(new byte[] { 0x00, 0x01 });
+        }
+
+        public ViscaClear(byte address)
+            : base(address)
+        {
+            Append(new byte[] { 0x00, 0x01});
+        }
+
+        public override string ToString()
+        {
+            return String.Format("Camera{0} Clear", this.Destination);
+        }
+    }
+}
