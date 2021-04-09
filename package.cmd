2a06260bbcbefde9bcb71909fbfe7998cb26f454
Maxim Batourine <m.batourine@gmail.com>
2021-04-09 15:18:55 -0400

    Update package.cmd

--- package.cmd
+++ package.cmd
@@ -1,2 +1,3 @@
 nuget pack ViscaLibrary\ViscaLibrary.nuspec -OutputDirectory ViscaLibrary\bin\Release
-nuget push ViscaLibrary\bin\Release\ViscaLibrary.1.0.99.nupkg  -Source https://api.nuget.org/v3/index.json
+nuget push ViscaLibrary\bin\Release\ViscaLibrary.1.1.0.nupkg  -Source https://api.nuget.org/v3/index.json
+nuget push ViscaLibrary\bin\Release\ViscaLibrary.1.1.0.nupkg  -Source "github"
